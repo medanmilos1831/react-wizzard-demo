4e20ec3643035f9bb556fe202e59941199637101
medanmilos1831 <medanmilos1831@gmail.com>
2025-08-29 15:10:17 +0200

    minnor change

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,5 @@
-// import { RouterProvider, createBrowserRouter } from "react-router-dom";
-// import { Layout } from "./components";
-
 import { HomePage } from "./homepage/HomePage";
 
-// Router configuration - commented out
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: [
-//       {
-//         index: true,
-//         element: <HomePage />,
-//       },
-//       {
-//         path: "about",
-//         element: <AboutPage />,
-//       },
-//       {
-//         path: "contact",
-//         element: <ContactPage />,
-//       },
-//     ],
-//   },
-// ]);
 
 function App() {
   return <HomePage />;
